5885f9a199e26105e9c2bef5060056be1a0586c0
shellus <shellus@endaosi.com>
2016-11-21 01:42:40 +0800

    更新示例

--- examples/shisuyun.php
+++ examples/shisuyun.php
@@ -5,51 +5,72 @@
  * Date: 2016-11-20
  * Time: 21:52
  */
-require './functions.php';
-require '../vendor/autoload.php';
+require __DIR__ . '/functions.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+class Shisuyun
+{
+    protected $initUrl = 'https://console.tenxcloud.com/login';
+    protected $browser;
+
+    public function init(){
+        $this -> browser = new Browser\Browser($this -> initUrl);
+    }
+    public function login(){
+        // 从首页html里面拿csrf token
+        $csrf_key = $this -> browser -> getCrawler() -> filter('meta[name="csrf-param"]') -> attr('content');
+        $csrf_value = $this -> browser -> getCrawler() -> filter('meta[name="csrf-token"]') -> attr('content');
+
+        // 拿到form元素，并填写账号密码
+        /** @var Browser\Form $from */
+        $from = $this -> browser -> form('[id=signup]');
+
+        $from -> setText('username', env('USERNAME'));
+
+        $from -> setText('password', env('PASSWORD'));
+
+        // 增加csrf的input
+        $from -> addText($csrf_key, $csrf_value);
+
+        // 提交表单
+        $from -> submit();
+
+        // 登陆后会跳转到首页，如果当前url不是首页，说明登录失败
+        if($this -> browser -> getCurrentUrl() !== "https://console.tenxcloud.com/"){
+            throw new Exception('login fail');
+        };
+    }
+    public function getMoney()
+    {
+        // 本来想通过DOM拿到余额，结果发现DOM上的金额是js赋值的。。
+        //$money = $browser -> getCrawler() -> filter('span[id="thisMonthconsumptionSum"]') ->text();
+
+        // 获取账户信息的接口
+        $infoUrl = 'https://console.tenxcloud.com/account/getconsumamount?_=' . time();
+
+        // 调用浏览器的方法去GET，会自动加上Cookies，Referer之类的信息
+        $json = $this -> browser -> request($infoUrl, 'GET');
+
+        $arr = json_decode($json, true);
+
+        $money = $arr[0]['balance'];
+
+        return $money;
+    }
+
+}
 
 
-$initUrl = 'https://console.tenxcloud.com/login';
+$shisuyun = new Shisuyun();
 
-$browser = new Browser\Browser();
+$shisuyun -> init();
 
-$browser -> go ($initUrl);
+$shisuyun -> login();
 
-// 从首页html里面拿csrf token
-$csrf_key = $browser -> crawler() -> filter('meta[name="csrf-param"]') -> attr('content');
-$csrf_value = $browser -> crawler() -> filter('meta[name="csrf-token"]') -> attr('content');
+var_dump($shisuyun -> getMoney());
 
-// 拿到form元素，并填写账号密码
-/** @var Browser\Form $from */
-$from = $browser -> form('[id=signup]');
 
-$from -> setText('username', env('USERNAME'));
 
-$from -> setText('password', env('PASSWORD'));
 
-// 增加csrf的input
-$from -> addText($csrf_key, $csrf_value);
 
-// 提交表单
-// browser window.location.href(xxx); FOLLOW LOCATION
-$from -> submit();
 
-// 登陆后会跳转到首页，如果当前url不是首页，说明登录失败
-if($browser -> url() !== "https://console.tenxcloud.com/"){
-    throw new Exception('login fail');
-};
-
-// 本来想通过DOM拿到余额，结果发现DOM上的金额是js赋值的。。
-//$money = $browser -> getCrawler() -> filter('span[id="thisMonthconsumptionSum"]') ->text();
-
-// 获取账户信息的接口
-$infoUrl = 'https://console.tenxcloud.com/account/getconsumamount?_=' . time();
-
-// 调用浏览器的方法去GET，会自动加上Cookies，Referer之类的信息
-$json = $browser -> request($infoUrl, 'GET');
-
-$arr = json_decode($json, true);
-
-$money = $arr[0]['balance'];
-
-var_dump($money);
